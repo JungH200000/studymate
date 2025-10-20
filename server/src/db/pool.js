530e1c4d557dfdc2b804465b8b53b142c6c2efbb
Jung20000 <jhpaxk02@naver.com>
2025-10-20 16:31:10 +0900

    feat: db 연결

---
+++ server/src/db/pool.js
@@ -0,0 +1,18 @@
+// src/db/pool.js
+// PostgreSQL(DB) 연결 설정
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export const query = (text, params) => pool.query(text, params);
+// query() 함수 생성
+// text: SQL 문 (`SELECT * FROM users WHERE id=$1`)
+// params: SQL에 들어갈 값 배열 (`[user_id]`)
+export default pool;
