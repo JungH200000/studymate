26af2dd05547223c6f50eac41215605a6d405c51
Jung20000 <jhpaxk02@naver.com>
2025-10-21 17:00:00 +0900

    feat: cors 설정 추가 및 error 수정

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,16 @@
 // src/server.js
 // 서버 시작점
 import express from 'express';
+import cors from 'cors';
 import { query } from './db/pool.js';
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+// origin : 다른 출처(origin)에서 오는 요청을 서버가 허용할지 결정
+// credentials : req에 쿠키, 인증 헤더 등 자격 증명이 포함된 요청을 할 수 있도록 허용
 
 /* ===== DB 연결 확인 ===== */
 try {
@@ -29,8 +33,10 @@ app.use('/api/auth', authRoutes);
 
 /* ===== Error ===== */
 app.use((error, req, res, next) => {
+  const status = error?.status || 500;
+  const message = status === 500 ? '서버에 오류가 발생했습니다.' : error.message || '요청을 처리할 수 없습니다.';
   console.error('Error 발생 !!!\n', error);
-  res.status(error.status).json({ message: error.message });
+  res.status(status).json({ ok: false, message });
 });
 
 app.listen(3000, () => {
