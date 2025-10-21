5fa3510a2bd40ecf6a9caca85d40726f7e832886
Jung20000 <jhpaxk02@naver.com>
2025-10-21 17:00:49 +0900

    feat: 비밀번호 검증 추가 및 필요없는 express 삭제

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,20 +1,14 @@
 // src/controllers/auth.controller.js
 // 요청-응답 처리
-
-import express from 'express';
 import { isEmail, isPassword, isUsername } from '../utils/auth.validators.js';
 import * as authService from '../services/auth.service.js';
-
-const app = express();
-
-app.use(express.json());
 
 /* ===== Register ===== */
 export const register = async (req, res) => {
   // 1) request에서 email, password, username 꺼냄
   let { email, password, username } = req.body;
 
-  if (!isEmail(email) || !isUsername(username)) {
+  if (!isEmail(email) || !isUsername(username) || !isPassword(password)) {
     console.log('email 또는 username이 잘못 입력됨');
     return res.status(400).json({ message: '잘못된 입력 형식입니다.' });
   }
