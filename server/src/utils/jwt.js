1a29d5f7511eca2431fa10ae9741829b1eeeb338
Jung20000 <jhpaxk02@naver.com>
2025-10-23 10:54:20 +0900

    refactor: 변수명 수정

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -5,20 +5,20 @@ import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
-export const signAccess = ({ user_id, email }) => {
+export const signAccess = ({ sub, email }) => {
   const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
   const ACCESS_EXPIRES = process.env.ACCESS_TOKEN_EXPIRES_IN;
 
-  const accessToken = jwt.sign({ user_id, email }, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRES });
+  const accessToken = jwt.sign({ sub, email }, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRES });
 
   return accessToken;
 };
 
-export const signRefresh = ({ user_id, jti }) => {
+export const signRefresh = ({ sub, jti }) => {
   const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
   const REFRESH_EXPIRES = process.env.REFRESH_TOKEN_EXPIRES_IN;
 
-  const refresh_token = jwt.sign({ user_id, jti }, REFRESH_SECRET, { expiresIn: REFRESH_EXPIRES });
+  const refreshToken = jwt.sign({ sub, jti }, REFRESH_SECRET, { expiresIn: REFRESH_EXPIRES });
 
-  return refresh_token;
+  return refreshToken;
 };
