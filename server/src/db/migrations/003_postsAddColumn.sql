03d63e9b9ba28e1969a8154aa386e4fe01084745
Jung20000 <jhpaxk02@naver.com>
2025-10-29 17:04:18 +0900

    feat: migration

---
+++ server/src/db/migrations/003_postsAddColumn.sql
@@ -0,0 +1,14 @@
+-- post table에 posted_date_kst column 추가 (25-10-29)
+
+BEGIN;
+
+ALTER TABLE posts 
+ADD posted_date_kst date
+NOT NULL
+DEFAULT (now() AT TIME ZONE 'Asia/Seoul')::date;
+
+ALTER TABLE posts
+ADD CONSTRAINT uq_posts_user_chal_kstday
+UNIQUE (user_id, challenge_id, posted_date_kst);
+
+COMMIT;
