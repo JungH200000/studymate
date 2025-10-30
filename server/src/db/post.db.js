6616295c5e15d43cf728749fa390ae2cbd72e5ad
Jung20000 <jhpaxk02@naver.com>
2025-10-30 14:14:23 +0900

    feat: 유저/챌린지별 참여 횟수 property 추가

--- server/src/db/post.db.js
+++ server/src/db/post.db.js
@@ -31,20 +31,18 @@ export async function verifyParticipation({ user_id, challenge_id }) {
 export async function duplicatedDailyPost({ user_id, challenge_id }) {
   // 날짜 계산은 DB에서!!!
   const sql = `
-    WITH kst AS (
-      SELECT (now() AT TIME ZONE 'Asia/Seoul')::date AS d
-    ) -- kst table을 만들고 d column을 만든 후 그 안에 오늘 kst 시간을 저장
-    SELECT 1
-    FROM posts p, kst
-    WHERE p.user_id = $1 AND p.challenge_id = $2
-      AND p.created_at >= (kst.d::timestamp AT TIME ZONE 'Asia/Seoul')
-      AND p.created_at < ((kst.d + INTERVAL '1 day')::timestamp AT TIME ZONE 'Asia/Seoul')`;
+    SELECT EXISTS (
+      SELECT 1
+      FROM posts
+      WHERE user_id = $1 AND challenge_id = $2
+        AND posted_date_kst = (now() AT TIME ZONE 'Asia/Seoul')::date
+    ) AS exists`;
   const params = [user_id, challenge_id];
 
-  const { rows, rowCount } = await query(sql, params);
+  const { rows } = await query(sql, params);
 
-  // `rowCount = false`이어야 daily 중복 인증글이 없는 것
-  return rowCount > 0;
+  // true면 중복
+  return rows[0].exists;
 }
 
 /** 인증글 등록 */
@@ -73,12 +71,44 @@ export async function countPost({ challenge_id }) {
   return rows[0].post_count;
 }
 
+/** 유저별/챌린지별 누적 참여 횟수(인증글 수) */
+export async function countMyPostsInChallenge({ user_id, challenge_id }) {
+  const sql = `
+    SELECT COUNT(*)::int AS my_post_count
+    FROM posts
+    WHERE user_id = $1 AND challenge_id = $2`;
+  const params = [user_id, challenge_id];
+
+  const { rows } = await query(sql, params);
+
+  return rows[0]?.my_post_count ?? 0;
+}
+
+/** 유저별/챌린지별 이번주 참여 횟수 */
+export async function countMyPostsThisWeek({ user_id, challenge_id }) {
+  // `date_trunc('week', ...)` : 변환된 시간을 기준으로 날짜를 주 단위로 자름
+  const sql = `
+    WITH kst AS (
+    SELECT (date_trunc('week', now() AT TIME ZONE 'Asia/Seoul'))::date AS week_start
+    )
+    SELECT COUNT(*)::int AS my_week_post_count
+    FROM posts p, kst
+    WHERE p.user_id = $1 AND p.challenge_id = $2
+      AND p.posted_date_kst >= kst.week_start
+      AND p.posted_date_kst < (kst.week_start + INTERVAL '7 day')::date;`;
+  const params = [user_id, challenge_id];
+
+  const { rows } = await query(sql, params);
+
+  return rows[0]?.my_week_post_count ?? 0;
+}
+
 /** 인증글 목록 가져오기 */
 export async function getPosts({ sort, limit, offset, challenge_id }) {
   let sql;
   if (sort === 'newest') {
     sql = `
-      SELECT p.*, u.user_id, u.username
+      SELECT p.*, u.user_id AS author_id, u.username AS author_username
       FROM posts p
       LEFT JOIN users u ON p.user_id = u.user_id
       WHERE p.challenge_id = $1
@@ -86,7 +116,7 @@ export async function getPosts({ sort, limit, offset, challenge_id }) {
       LIMIT $2 OFFSET $3`;
   } else {
     sql = `
-      SELECT p.*, u.user_id, u.username
+      SELECT p.*, u.user_id AS author_id, u.username AS author_username
       FROM posts p
       LEFT JOIN users u ON p.user_id = u.user_id
       WHERE p.challenge_id = $1
